263cd1f2344fc1529ecfc3d392ed702c451ebedb
Gabrielle Malate <gabriellemalate@gmail.com>
2023-12-07 21:49:26 -0800

    show list label row for tablet and desktop

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -2,57 +2,71 @@ function generateShowsSection() {
     const showsContainer = document.getElementById('showsContainer');
 
     const showsData = [
-        {
-            date: '2021-09-06',
-            venue: 'Ronald Lane',
-            location: 'San Francisco, CA',
-            link: '#'
-        },
-        {
-            date: '2021-09-21',
-            venue: 'Pier 3 East',
-            location: 'San Francisco, CA',
-            link: '#'
-        },
-        {
-            date: '2021-10-15',
-            venue: 'View Lounge',
-            location: 'San Francisco, CA',
-            link: '#'
-        },
-        {
-            date: '2021-11-06',
-            venue: 'Hyatt Agency',
-            location: 'San Francisco, CA',
-            link: '#'
-        },
-        {
-            date: '2021-11-26',
-            venue: 'Moscow Center',
-            location: 'San Francisco, CA',
-            link: '#'
-        },
-        {
-            date: '2021-12-15',
-            venue: 'Press Club',
-            location: 'San Francisco, CA',
-            link: '#'
-        }
+        { date: '2021-09-06', venue: 'Ronald Lane', location: 'San Francisco, CA', link: '#' },
+        { date: '2021-09-21', venue: 'Pier 3 East', location: 'San Francisco, CA', link: '#' },
+        { date: '2021-10-15', venue: 'View Lounge', location: 'San Francisco, CA', link: '#' },
+        { date: '2021-11-06', venue: 'Hyatt Agency', location: 'San Francisco, CA', link: '#' },
+        { date: '2021-11-26', venue: 'Moscow Center', location: 'San Francisco, CA', link: '#' },
+        { date: '2021-12-15', venue: 'Press Club', location: 'San Francisco, CA', link: '#' }
     ];
 
+    // shows section
     const showsSection = document.createElement('section');
     showsSection.classList.add('shows');
 
+    // shows all container
     const showsAllDiv = document.createElement('div');
     showsAllDiv.classList.add('shows__all');
 
+    // heading
     const h2 = document.createElement('h2');
     h2.classList.add('shows__all--heading');
     h2.textContent = 'Shows';
 
+    // container for shows
     const showsContainerDiv = document.createElement('div');
     showsContainerDiv.classList.add('shows__container');
 
+    // invisible row for labels
+    const invisibleRowDiv = document.createElement('div');
+    invisibleRowDiv.classList.add('shows__invisible-row');
+
+    const invisibleRowEqDiv = document.createElement('div');
+    invisibleRowEqDiv.classList.add('shows__invisible-row--eq');
+
+    const dateLabelH3 = document.createElement('h3');
+    dateLabelH3.classList.add('shows__invisible-row--label');
+    dateLabelH3.textContent = 'DATE';
+
+    const invisibleRowVenloDiv = document.createElement('div');
+    invisibleRowVenloDiv.classList.add('shows__invisible-row--venlo');
+
+    const venueLabelH3 = document.createElement('h3');
+    venueLabelH3.classList.add('shows__invisible-row--label');
+    venueLabelH3.textContent = 'VENUE';
+
+    const locationLabelH3 = document.createElement('h3');
+    locationLabelH3.classList.add('shows__invisible-row--label');
+    locationLabelH3.textContent = 'LOCATION';
+
+    invisibleRowVenloDiv.appendChild(venueLabelH3);
+    invisibleRowVenloDiv.appendChild(locationLabelH3);
+
+    invisibleRowEqDiv.appendChild(dateLabelH3);
+    invisibleRowEqDiv.appendChild(invisibleRowVenloDiv);
+
+    invisibleRowDiv.appendChild(invisibleRowEqDiv);
+
+    // Accessibility label for buy buttons
+    const accessibilityLabelP = document.createElement('p');
+    accessibilityLabelP.classList.add('shows__invisible-row--accessibility-label');
+    accessibilityLabelP.textContent = 'BUY BUTTONS';
+
+    invisibleRowDiv.appendChild(accessibilityLabelP);
+
+    showsContainerDiv.appendChild(invisibleRowDiv);
+
+    // show articles
     showsData.forEach(show => {
         const showArticle = document.createElement('article');
         showArticle.classList.add('shows__show');
@@ -60,6 +74,7 @@ function generateShowsSection() {
         const eqDiv = document.createElement('div');
         eqDiv.classList.add('shows__eq');
 
+        // Date
         const dateH3 = document.createElement('h3');
         dateH3.classList.add('shows__gone');
         dateH3.textContent = 'DATE';
@@ -69,6 +84,7 @@ function generateShowsSection() {
         dateElement.setAttribute('datetime', show.date);
         dateElement.textContent = new Date(show.date).toDateString();
 
+        // Venue and Location
         const venloDiv = document.createElement('div');
         venloDiv.classList.add('shows__eq--venlo');
 
@@ -88,6 +104,12 @@ function generateShowsSection() {
         locationPar.classList.add('shows__show--par');
         locationPar.textContent = show.location;
 
+        venloDiv.appendChild(venueH3);
+        venloDiv.appendChild(venuePar);
+        venloDiv.appendChild(locationH3);
+        venloDiv.appendChild(locationPar);
+
+        // Buy Tickets Button
         const button = document.createElement('button');
         button.classList.add('shows__show--button');
         const link = document.createElement('a');
@@ -95,11 +117,6 @@ function generateShowsSection() {
         link.title = 'Only few left! Buy now!';
         link.textContent = 'BUY TICKETS';
         button.appendChild(link);
-
-        venloDiv.appendChild(venueH3);
-        venloDiv.appendChild(venuePar);
-        venloDiv.appendChild(locationH3);
-        venloDiv.appendChild(locationPar);
 
         eqDiv.appendChild(dateH3);
         eqDiv.appendChild(dateElement);
@@ -111,9 +128,12 @@ function generateShowsSection() {
         showsContainerDiv.appendChild(showArticle);
     });
 
+    // Append elements to the shows section
     showsAllDiv.appendChild(h2);
     showsAllDiv.appendChild(showsContainerDiv);
     showsSection.appendChild(showsAllDiv);
+
+    // Append the shows section to the container
     showsContainer.appendChild(showsSection);
 }
 

5b56bc9c73dfa84e6c08d4d4190a27a54bd076ff
Gabrielle Malate <gabriellemalate@gmail.com>
2023-12-08 15:01:15 -0800

    new comment posts on top of old comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,138 +1,127 @@
-// Function that generates the comments section
-function generateCommentsSection() {
-    const commentsContainer = document.getElementById('commentsContainer');
-
-    const commentsData = [
+// Initialize old comments array
+    const oldComments = [
         { name: 'Connor Walton', date: '2021-02-17', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
         { name: 'Emilie Beach', date: '2021-01-09', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' },
         { name: 'Miles Acosta', date: '2020-12-20', comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
     ];
 
-    const commentsSection = document.createElement('section');
-    commentsSection.classList.add('comment');
+    // Create comments section
+    function generateCommentsSection() {
+        const commentsContainer = document.getElementById('commentsContainer');
 
-    const commentsAllDiv = document.createElement('div');
-    commentsAllDiv.classList.add('comment__all');
+        // Create main section
+        const commentsSection = document.createElement('section');
+        commentsSection.classList.add('comment');
 
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Join the Conversation';
+        // Create comment__all container
+        const commentsAllDiv = document.createElement('div');
+        commentsAllDiv.classList.add('comment__all');
 
-    const commentsBoxesDiv = document.createElement('div');
-    commentsBoxesDiv.classList.add('comment__all--boxes');
+        // Create heading
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Join the Conversation';
 
-    // Add new comment form
-    const newCommentArticle = document.createElement('article');
-    newCommentArticle.classList.add('comment__new');
+        // Create comment__all--boxes container
+        const commentsBoxesDiv = document.createElement('div');
+        commentsBoxesDiv.classList.add('comment__all--boxes');
 
-    const newCommentDiv = document.createElement('div');
-    newCommentDiv.classList.add('comment__entirely');
+        // Add new comment form
+        const newCommentArticle = createNewCommentArticle();
 
-    const newCommentImg = document.createElement('img');
-    newCommentImg.classList.add('comment__new--img');
-    newCommentImg.setAttribute('src', './assets/Images/Mohan-muruge.jpg');
-    newCommentImg.setAttribute('alt', "Mohan's Photo");
+        // Add form to boxes container
+        commentsBoxesDiv.appendChild(newCommentArticle);
 
-    const newCommentForm = document.createElement('form');
-    newCommentForm.classList.add('comment__entirely--text');
-    newCommentForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        postComment(newCommentForm);
-    });
+        // Add hr after the new comment form
+        const hrAfterForm = document.createElement('hr');
+        commentsBoxesDiv.appendChild(hrAfterForm);
 
-    const nameLabel = document.createElement('h4');
-    nameLabel.textContent = 'NAME';
+        // Add new-posted section
+        const newPostedSection = document.createElement('article');
+        newPostedSection.classList.add('comment__new-posted');
+        commentsBoxesDiv.appendChild(newPostedSection);
 
-    const nameTextarea = document.createElement('textarea');
-    nameTextarea.classList.add('comment__new--name');
-    nameTextarea.setAttribute('type', 'text');
-    nameTextarea.setAttribute('placeholder', 'Enter your name');
-    nameTextarea.style.resize = 'none';
+        // Add hr after new-posted section
+        const hrAfterNewPosted = document.createElement('hr');
+        commentsBoxesDiv.appendChild(hrAfterNewPosted);
 
-    const commentLabel = document.createElement('h4');
-    commentLabel.textContent = 'COMMENT';
+        // Add old comments to boxes container
+        oldComments.forEach(comment => {
+            const oldCommentArticle = createOldCommentArticle(comment);
+            commentsBoxesDiv.appendChild(oldCommentArticle);
 
-    const commentTextarea = document.createElement('textarea');
-    commentTextarea.classList.add('comment__new--comment');
-    commentTextarea.setAttribute('type', 'text');
-    commentTextarea.setAttribute('placeholder', 'Add a new comment');
-    commentTextarea.style.resize = 'none';
+            // Add hr after each old comment
+            const hrAfterOldComment = document.createElement('hr');
+            commentsBoxesDiv.appendChild(hrAfterOldComment);
+        });
 
-    const submitButton = document.createElement('button');
-    submitButton.setAttribute('type', 'submit');
-    submitButton.textContent = 'COMMENT';
+        // Add heading and boxes container to comment__all container
+        commentsAllDiv.appendChild(h2);
+        commentsAllDiv.appendChild(commentsBoxesDiv);
 
-    newCommentForm.appendChild(nameLabel);
-    newCommentForm.appendChild(nameTextarea);
-    newCommentForm.appendChild(commentLabel);
-    newCommentForm.appendChild(commentTextarea);
-    newCommentForm.appendChild(submitButton);
+        // Add comment__all container to main section
+        commentsSection.appendChild(commentsAllDiv);
 
-    newCommentDiv.appendChild(newCommentImg);
-    newCommentDiv.appendChild(newCommentForm);
-    newCommentArticle.appendChild(newCommentDiv);
+        // Add main section to comments container
+        commentsContainer.appendChild(commentsSection);
+    }
 
-    commentsBoxesDiv.appendChild(newCommentArticle);
-
-    // Add horizontal line after each comment
-    const addHorizontalLine = () => {
-        const hr = document.createElement('hr');
-        commentsBoxesDiv.appendChild(hr);
-    };
-
-    function postComment(form) {
-        const nameInput = form.querySelector('.comment__new--name');
-        const commentInput = form.querySelector('.comment__new--comment');
-
-        // Get the current date
-        const currentDate = new Date().toLocaleDateString();
-
-        // Create a new comment article
+    // Function to create the new comment form
+    function createNewCommentArticle() {
         const newCommentArticle = document.createElement('article');
-        newCommentArticle.classList.add('comment__old');
+        newCommentArticle.classList.add('comment__new');
 
         const newCommentDiv = document.createElement('div');
         newCommentDiv.classList.add('comment__entirely');
 
         const newCommentImg = document.createElement('img');
-        newCommentImg.setAttribute('src', './assets/Images/empty-image.png');
-        newCommentImg.setAttribute('alt', `${nameInput.value}'s Photo`);
+        newCommentImg.classList.add('comment__new--img');
+        newCommentImg.setAttribute('src', './assets/Images/Mohan-muruge.jpg');
+        newCommentImg.setAttribute('alt', "Mohan's Photo");
 
-        const newTextDiv = document.createElement('div');
-        newTextDiv.classList.add('comment__entirely--text');
+        const newCommentForm = document.createElement('form');
+        newCommentForm.classList.add('comment__entirely--text');
+        newCommentForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            postComment(newCommentForm);
+        });
 
-        const newTopDiv = document.createElement('div');
-        newTopDiv.classList.add('comment__old--top');
+        const nameLabel = document.createElement('h4');
+        nameLabel.textContent = 'NAME';
 
-        const newNameH3 = document.createElement('h3');
-        newNameH3.textContent = nameInput.value;
+        const nameTextarea = document.createElement('textarea');
+        nameTextarea.classList.add('comment__new--name');
+        nameTextarea.setAttribute('type', 'text');
+        nameTextarea.setAttribute('placeholder', 'Enter your name');
+        nameTextarea.style.resize = 'none';
 
-        const newDateElement = document.createElement('time');
-        newDateElement.setAttribute('datetime', currentDate);
-        newDateElement.textContent = currentDate;
+        const commentLabel = document.createElement('h4');
+        commentLabel.textContent = 'COMMENT';
 
-        const newCommentP = document.createElement('p');
-        newCommentP.textContent = commentInput.value;
+        const commentTextarea = document.createElement('textarea');
+        commentTextarea.classList.add('comment__new--comment');
+        commentTextarea.setAttribute('type', 'text');
+        commentTextarea.setAttribute('placeholder', 'Add a new comment');
+        commentTextarea.style.resize = 'none';
 
-        newTopDiv.appendChild(newNameH3);
-        newTopDiv.appendChild(newDateElement);
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.textContent = 'COMMENT';
 
-        newTextDiv.appendChild(newTopDiv);
-        newTextDiv.appendChild(newCommentP);
+        newCommentForm.appendChild(nameLabel);
+        newCommentForm.appendChild(nameTextarea);
+        newCommentForm.appendChild(commentLabel);
+        newCommentForm.appendChild(commentTextarea);
+        newCommentForm.appendChild(submitButton);
 
         newCommentDiv.appendChild(newCommentImg);
-        newCommentDiv.appendChild(newTextDiv);
-
+        newCommentDiv.appendChild(newCommentForm);
         newCommentArticle.appendChild(newCommentDiv);
-        commentsBoxesDiv.insertBefore(newCommentArticle, newCommentArticle.nextSibling);
 
-        // Clear the input fields
-        nameInput.value = '';
-        commentInput.value = '';
-
-        addHorizontalLine();
+        return newCommentArticle;
     }
 
-    commentsData.forEach(comment => {
+    // Function to create an old comment
+    function createOldCommentArticle(comment) {
         const oldCommentArticle = document.createElement('article');
         oldCommentArticle.classList.add('comment__old');
 
@@ -169,16 +158,34 @@ function generateCommentsSection() {
         oldCommentDiv.appendChild(textDiv);
 
         oldCommentArticle.appendChild(oldCommentDiv);
-        commentsBoxesDiv.appendChild(oldCommentArticle);
 
-        addHorizontalLine();
-    });
+        return oldCommentArticle;
+    }
 
-    commentsAllDiv.appendChild(h2);
-    commentsAllDiv.appendChild(commentsBoxesDiv);
-    commentsSection.appendChild(commentsAllDiv);
-    commentsContainer.appendChild(commentsSection);
-}
+    // Function to handle posting new comments
+    function postComment(form) {
+        const nameInput = form.querySelector('.comment__new--name');
+        const commentInput = form.querySelector('.comment__new--comment');
 
-// Call the function to generate the comments section
-generateCommentsSection();
+        // Get the current date
+        const currentDate = new Date().toLocaleDateString();
+
+        // Create a new comment object
+        const newComment = {
+            name: nameInput.value,
+            date: currentDate,
+            comment: commentInput.value
+        };
+
+        // Add the new comment to the new-posted section
+        const newPostedSection = document.querySelector('.comment__new-posted');
+        const newPostedCommentArticle = createOldCommentArticle(newComment);
+        newPostedSection.insertBefore(newPostedCommentArticle, newPostedSection.firstChild);
+
+        // Clear the input fields
+        nameInput.value = '';
+        commentInput.value = '';
+    }
+
+    // Call the function to generate the comments section
+    generateCommentsSection();
